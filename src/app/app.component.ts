baf35f90bb99a63fff8f283e98fdbbf78bcaf816
edim dendy <edim@softwareseni.com>
2019-11-20 04:41:26 +0700

    Input Binding Custom Properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipe-book-udemy';
+  serverElements = [
+    { type: 'server', name: 'test name', content: 'balala' }
+  ];
 }
