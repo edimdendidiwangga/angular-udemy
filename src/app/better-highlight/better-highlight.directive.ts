12cebf2f9888633ef608d95e948f4178b9068fc7
edim dendy <edim@softwareseni.com>
2019-11-21 16:25:50 +0700

    using HostListener to Listen to Host Properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,17 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  Renderer2,
+  ElementRef,
+  HostListener,
+  HostBinding
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
@@ -13,10 +21,15 @@ export class BetterHighlightDirective implements OnInit {
 
   @HostListener('mouseover') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    setTimeout(() => {
+      this.backgroundColor = 'red';
+    }, 1000);
+    
   }
 
   @HostListener('mouseout') mouseleave(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 
 }
