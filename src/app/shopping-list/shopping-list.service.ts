e062c550e0dfb52d3558168f8e9b09e2dd73637e
edim dendy <edim@softwareseni.com>
2019-11-23 16:56:01 +0700

    Adding the shopping List Service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,4 +5,12 @@ export class ShoppingListService {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
   ];
+
+  getIngredients() {
+    return this.ingredients;
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
