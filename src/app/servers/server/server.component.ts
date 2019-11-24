238cb3ebeea496a32d3da338c59d498264d93dda
edim dendy <edim@softwareseni.com>
2019-11-25 06:37:09 +0700

    Practicing and some Common Gotchas

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -17,9 +17,7 @@ export class ServerComponent implements OnInit {
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe((params: Params) => {
-      if (params['id']) {
-        this.server = this.serversService.getServer(+params['id']);
-      }
+      this.server = this.serversService.getServer(+params['id']);
     })
   }
 
