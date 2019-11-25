4e517cec28c8a6d61db0b7acb0abd28b42f77b2a
edim dendy <edim@softwareseni.com>
2019-11-25 16:21:43 +0700

    Setting Up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
