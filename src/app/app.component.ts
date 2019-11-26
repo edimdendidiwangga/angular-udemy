55d1048c0f4e42604b78f88e9f30b210ecf20bb0
edim dendy <edim@softwareseni.com>
2019-11-26 13:58:30 +0700

    Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'recipe-book-udemy';
+  userActivated = false;
+  private activatedSub: Subscription;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(didActive => {
+      this.userActivated = didActive;
+    })
+  }
+
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe();
+  }
+
 }
