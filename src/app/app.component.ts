a1f13b98caa66262c260025138030a24cfb5fe4b
edim dendy <edim@softwareseni.com>
2019-11-27 08:51:36 +0700

    TD: Submitting and Using the Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,10 @@ export class AppComponent {
   title = 'recipe-book-udemy';
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('submit', form);
+    
   }
 }
