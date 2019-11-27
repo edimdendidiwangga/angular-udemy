ebb4803f2d2861c5accdae2e2f019b3044a2c320
edim dendy <edim@softwareseni.com>
2019-11-28 06:02:13 +0700

    Reactive: Syncing HTML and Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,23 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('f', {static: false}) signupForm: NgForm;
+export class AppComponent implements OnInit {
   title = 'recipe-book-udemy';
-  defaultQuestion = "pet";
-  answer = '';
   genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  }
-  submitted = false;
-  
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.signupForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'male'
-    // })
-    this.signupForm.form.patchValue({
-      userData: {
-        username: suggestedName,
-        email: ''
-      },
+  signupForm: FormGroup
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male'),
+
     })
   }
 
-  // onSubmit(form: NgForm) {
-  //   console.log('submit', form);
-  // }
-  onSubmit(form: NgForm) {
-    this.submitted = true;
-    const { value } = this.signupForm;
-    this.user.username = value.userData.username;
-    this.user.email = value.userData.email;
-    this.user.secretQuestion = value.secret;
-    this.user.answer = value.questionAnswer;
-    this.user.gender = value.gender;
-
-    this.signupForm.reset();
-  }
 }
