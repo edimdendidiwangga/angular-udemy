56026c83bad59fa03edac82dad7265486fbdcaad
edim dendy <edim@softwareseni.com>
2019-11-30 10:40:21 +0700

    Using Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipe-book-udemy';
-}
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
   servers = [
     {
       instanceType: 'medium',
