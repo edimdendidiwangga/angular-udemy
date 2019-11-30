f6f08abd625139b8fe427b44785622e97a4f69af
edim dendy <edim@softwareseni.com>
2019-11-30 16:37:10 +0700

    Using Subjects for Error Handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostService } from './post.service';
 import { post } from 'selenium-webdriver/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postService: PostService) {}
 
   ngOnInit() {
+    this.errorSub = this.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    })
     this.onFetchPosts();
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
    this.postService.createAndStorePost(postData.title, postData.content)
-    .subscribe(responseData => {
-      console.log(responseData);
-      this.loadedPosts.push(postData)
-    });
+    // .subscribe(responseData => {
+    //   console.log(responseData);
+    //   this.loadedPosts.push(postData)
+    // });
   }
 
   onFetchPosts() {
@@ -47,5 +52,9 @@ export class AppComponent implements OnInit {
     this.postService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 }
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -2,21 +2,26 @@ import { Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { Subject } from 'rxjs';
 
 @Inject({
   providedIn: 'root'
 })
 export class PostService {
   baseUrl = 'https://myapps-2c658.firebaseio.com/posts.json';
+  error = new Subject<string>();
+
   constructor(private http: HttpClient) {}
 
   createAndStorePost(title: string, content: string) {
     const postData: Post = { title, content };
     return this.http
     .post<{ name: string }>(this.baseUrl, postData)
-    // .subscribe(responseData => {
-    //   console.log(responseData);
-    // });
+    .subscribe(responseData => {
+      console.log(responseData);
+    }, error => {
+      this.error.next(error.message);
+    });
   }
 
   fetchPosts() {
