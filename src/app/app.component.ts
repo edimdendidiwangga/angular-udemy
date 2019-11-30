f1366636fa19de152e87b1950b39b40caa8dd264
edim dendy <edim@softwareseni.com>
2019-11-30 15:31:48 +0700

    Outputting Posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -43,6 +43,7 @@ export class AppComponent implements OnInit {
         return postsArray
       }))
       .subscribe(responseData => {
+        this.loadedPosts = responseData
         console.log(responseData);
       });
   }
