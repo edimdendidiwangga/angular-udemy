bcdb2e63091bf074cd300cff1019c58fc14a239d
edim dendy <edim@softwareseni.com>
2019-12-01 04:59:57 +0700

    Storing Recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,13 @@ export class HeaderComponent {
   isCollapse = false;
   @Output() featuredSelected = new EventEmitter<string>()
 
+  constructor(private dataStorageService: DataStorageService) { }
+
   onSelect(feature: string) {
     this.featuredSelected.emit(feature)
+  }
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
+import { Recipe } from '../recipes/recipes.model';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {
+  baseUrl = 'https://myapps-2c658.firebaseio.com/recipes.json';
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http.put(this.baseUrl, recipes).subscribe((response) => {
+      console.log('data', response)
+    });
   }
 }
