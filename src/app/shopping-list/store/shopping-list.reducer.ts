40dbe084e3fd3474f54816e8105e035d1b395369
edim dendy <edim@softwareseni.com>
2019-12-07 17:14:34 +0700

    Updating & Deleting Ingredients

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -21,6 +21,19 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
         ...state,
         ingredients: [...state.ingredients, ...action.payload]
       };
+
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const updatedIngredients = state.ingredients
+      updatedIngredients[action.payload.index] = action.payload.ingredient;
+      return {
+        ...state,
+        ingredients: updatedIngredients
+      };
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter((ing, index) => index !== action.payload)
+      };
     default:
       return state;
   }
