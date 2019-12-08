79d7820af50c48f75c9b300c1c3835e33b9d1151
edim dendy <edim@softwareseni.com>
2019-12-09 06:36:43 +0700

    Finishing the Auth Effects

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,28 +1,13 @@
 import { Injectable } from "@angular/core";
-import { environment } from '../../environments/environment';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
-
-export interface AuthResponseSata {
-  kind: string;
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  expiresIn: string;
-  localId: string;
-  registered?: boolean;
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private tokenExpirationTimer: any;
-
-  baseUrl(type) {
-    return `https://identitytoolkit.googleapis.com/v1/accounts:${type}?key=${environment.firebaseAPIKey}`;
-  }
 
   constructor(private store: Store<fromApp.AppState>) { }
   
