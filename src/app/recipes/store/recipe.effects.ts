29f65bde178ccf636f60323487a871a015900796
edim dendy <edim@softwareseni.com>
2019-12-12 05:59:45 +0700

    Fetching Recipes & Using the Resolver

---
+++ src/app/recipes/store/recipe.effects.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { switchMap, map } from 'rxjs/operators';
+
+import * as RecipesActions from './recipe.actions';
+import { Recipe } from '../recipes.model';
+
+@Injectable()
+export class RecipeEffects {
+  baseUrl(token) { return 'https://myapps-2c658.firebaseio.com/recipes.json' + token };
+  
+  @Effect()
+  fetchRecipes = this.actions$.pipe(
+    ofType(RecipesActions.FETCH_RECIPES),
+    switchMap(() => {
+      return this.http.get<Recipe[]>(this.baseUrl(''))
+    }),
+    map(recipes => {
+      return recipes.map(recipe => {
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients ? recipe.ingredients : []
+        };
+      });
+    }),
+    map(recipes => {
+      return new RecipesActions.SetRecipes(recipes);
+    })
+  );
+
+  constructor(private actions$: Actions, private http: HttpClient) {}
+}
