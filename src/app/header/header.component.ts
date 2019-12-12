c9208a3b87465647df2c2e774947f3037213b6c6
edim dendy <edim@softwareseni.com>
2019-12-12 14:25:52 +0700

    Storing Recipes via Effects

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
-import { DataStorageService } from '../shared/data-storage.service';
+
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions';
 import * as RecipeActions from '../recipes/store/recipe.actions';
@@ -16,7 +16,6 @@ export class HeaderComponent implements OnInit, OnDestroy{
   isAuthenticated = false;
 
   constructor(
-    private dataStorageService: DataStorageService,
     private store: Store<fromApp.AppState>
   ) { }
 
@@ -35,7 +34,8 @@ export class HeaderComponent implements OnInit, OnDestroy{
   }
 
   onSaveData() {
-    this.dataStorageService.storeRecipes();
+    // this.dataStorageService.storeRecipes();
+    this.store.dispatch(new RecipeActions.StoreRecipes());
   }
   onFetchData() {
     // this.dataStorageService.fetchRecipes().subscribe();
--- src/app/recipes/store/recipe.actions.ts
+++ src/app/recipes/store/recipe.actions.ts
@@ -6,7 +6,7 @@ export const FETCH_RECIPES = '[Recipes] Fetch Recipes';
 export const ADD_RECIPE = '[Recipes] Add Recipe';
 export const UPDATE_RECIPE = '[Recipes] Update Recipe';
 export const DELETE_RECIPES = '[Recipes] Delete Recipe';
-
+export const STORE_RECIPES = '[Recipes] Store Recipe';
 
 export class SetRecipes implements Action {
   readonly type = SET_RECIPES;
@@ -36,9 +36,14 @@ export class DeleteRecipes implements Action {
   constructor(public payload: number) {}
 }
 
+export class StoreRecipes implements Action {
+  readonly type = STORE_RECIPES;
+}
+
 export type RecipesActions = 
   | SetRecipes
   | FetchRecipes
   | AddRecipes
   | UpdateRecipes
-  | DeleteRecipes;
+  | DeleteRecipes
+  | StoreRecipes;
